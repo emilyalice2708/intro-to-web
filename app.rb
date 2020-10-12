ed0dc19cd9b6be2f6b2e3d66a610bc2509ab0540
Emily Sesto <emilyalicesesto@gmail.com>
2020-10-12 22:59:00 +0100

    First use of html image tag in /cat

--- app.rb
+++ app.rb
@@ -1,9 +1,16 @@
 require 'sinatra'
+require 'shotgun'
+
+set :session_secret, 'super secret'
 
 get '/' do
   'Hello world!'
 end
 
 get '/secret' do
-  'I am a secret...'
+  'I am a secret...!'
+end
+
+get '/cat' do
+  "<img src='http://bit.ly/1eze8aE'>"
 end
